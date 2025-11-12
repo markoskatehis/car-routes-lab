83587c554186e225aea09b85249033c1334b405f
Markos <markoskatehis@gmail.com>
2025-11-12 06:41:29 -0500

    Lab submission

--- server/app.py
+++ server/app.py
@@ -1,1 +1,19 @@
+from flask import Flask
+
+app = Flask(__name__)
+
 existing_models = ['Beedle', 'Crossroads', 'M2', 'Panique']
+
+@app.route("/")
+def home():
+    return "Welcome to Flatiron Cars"
+
+@app.route("/<model>")
+def model_lookup(model):
+    if model in existing_models:
+        return f"Flatiron {model} is in our fleet!"
+    else:
+        return f"No models called {model} exists in our catalog"
+    
+if __name__ == "__main__":
+    app.run(debug=True)
